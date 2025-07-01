30f741e42cc745ff7094b8143690fba8fdaee85a
Sandres92 <eastoutcity@mail.ru>
2025-07-01 19:38:24 +0000

    Add project

---
+++ test_allocator.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE test_version
+
+#include "lib.h"
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(test_allocator)
+
+BOOST_AUTO_TEST_CASE(test_valid_version)
+{
+	BOOST_CHECK(version() > 0);
+}
+
+BOOST_AUTO_TEST_CASE(test_valid_sum)
+{
+	BOOST_CHECK(1 + 1 == 2);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
