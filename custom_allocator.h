f50acd86d0afa388b77e022920698588b4b9171d
Sandres92 <eastoutcity@mail.ru>
2025-07-26 17:48:06 +0000

    Add custom allocator

--- custom_allocator.h
+++ custom_allocator.h
@@ -1,13 +1,58 @@
 #pragma once
+#include <iostream>
 
 namespace otus
 {
+    template <class T, size_t size>
     class CustomAllocator
     {
+        using value_type = T;
+
+        using pointer = T *;
+        using const_pointer = const T *;
+        using reference = T &;
+        using const_reference = const T &;
+
+        CustomAllocator() : current(0) = default;
+
+        template <class U>
+        CustomAllocator(const CustomAllocator<T> &other) = default;
+
+        T *allocate(std::size_t n)
+        {
+            if (n == 0)
+            {
+                std::invalid_argument("invalid_argument size alloc");
+            }
+
+            if (position + n > size)
+            {
+                std::bad_alloc();
+            }
+
+            T *p = reinterpret_cast<T *>(&buffer[position]);
+            position += n;
+            return p;
+        }
+
+        void deallocate(T *p, std::size_t n)
+        {
+        }
+
+        template <typename U, typename... Args>
+        void construct(U *p, Args &&...args)
+        {
+            new (p) U(std::forward<Args>(args)...);
+        };
+
+        template <typename U>
+        void destroy(U *p)
+        {
+            p->~U();
+        }
+
     private:
-        /* data */
-    public:
-        CustomAllocator(/* args */);
-        ~CustomAllocator();
+        alignas(T) char buffer[size * sizeof(T)];
+        size_t position = 0;
     };
 }
--- main.cpp
+++ main.cpp
@@ -2,11 +2,18 @@
 
 #include "lib.h"
 #include "custom_allocator.h"
+#include <map>
 
 int main(int, char **)
 {
   std::cout << "Version: " << version() << std::endl;
   std::cout << "Hello, world!" << std::endl;
+  std::map<int, int> m1;
+
+  std::map<int, int,
+           std::less<>,
+           otus::CustomAllocator<std::pair<int, int>, 10>>
+      m2;
 
   return 0;
 }
